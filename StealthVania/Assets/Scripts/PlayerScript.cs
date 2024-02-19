1323cfb9427d471f334c7b9eeaf9d3ea4abeaed7
ivan ramirez <ixr4077@mavs.uta.edu>
2024-02-19 17:25:25 -0600

    Dash working

--- StealthVania/Assets/Scripts/PlayerScript.cs
+++ StealthVania/Assets/Scripts/PlayerScript.cs
@@ -7,6 +7,7 @@ public class PlayerScript : MonoBehaviour
     [SerializeField] public Rigidbody2D PlayerBody;
     [SerializeField] public Transform groundCheck;
     [SerializeField] public LayerMask groundLayer;
+    [SerializeField] private TrailRenderer tr;
 
     public float speed;
     public float move;
@@ -14,10 +15,21 @@ public class PlayerScript : MonoBehaviour
     public bool grounded;
     private bool isFacingRight = true;
 
+    //Dashing values
+    public float dashStrength;
+    public float dashingTime;
+    public float dashingCooldown;
+    private bool canDash = true;
+    private bool isDashing =  false;
+
 
     // Update is called once per frame.
     void Update()
     {
+        if (isDashing)
+        {
+            return;
+        }
         move = Input.GetAxisRaw("Horizontal");
         PlayerBody.velocity = new Vector2(move * speed, PlayerBody.velocity.y);
         if (Input.GetKeyDown(KeyCode.Space) && isGrounded()) {
@@ -27,6 +39,11 @@ public class PlayerScript : MonoBehaviour
         {
             PlayerBody.velocity = new Vector2(PlayerBody.velocity.x, PlayerBody.velocity.y * 0.5f);
         }
+        if (Input.GetKeyDown(KeyCode.Q) && canDash)
+        {
+            StartCoroutine(Dash());
+        }
+
         Flip();
     }
     private bool isGrounded()
@@ -38,10 +55,27 @@ public class PlayerScript : MonoBehaviour
     {
         if (isFacingRight && move > 0f || !isFacingRight && move < 0f)
         {
-            isFacingRight = !isFacingRight;
             Vector3 localScale = transform.localScale;
+            isFacingRight = !isFacingRight;
             localScale.x *= -1f;
             transform.localScale = localScale;
         }
+    }
+    
+    private IEnumerator Dash()
+    {
+        canDash = false;
+        isDashing = true;
+        float originalGravity = PlayerBody.gravityScale;
+        PlayerBody.gravityScale = 0f;
+        PlayerBody.velocity = new Vector2(transform.localScale.x * -dashStrength, 0f);
+        tr.emitting = true;
+        yield return new WaitForSeconds(dashingTime);
+        isDashing = false;
+        PlayerBody.gravityScale = originalGravity;
+        tr.emitting = false;
+        yield return new WaitForSeconds(dashingCooldown);
+        canDash = true;
+        
     }
 }

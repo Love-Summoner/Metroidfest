8b0e1f9174eb295bdd108724b0a9a8e91e8ffb39
ivan ramirez <ixr4077@mavs.uta.edu>
2024-03-13 18:36:02 -0500

    Turret in progress

---
+++ StealthVania/Assets/Scripts/TurretBulletScript.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurretBulletScript : MonoBehaviour
+{
+    [SerializeField] private GameObject player;
+    private Rigidbody2D rb;
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        player = GameObject.FindGameObjectWithTag("Player");
+
+        Vector3 direction = player.transform.position - transform.position;
+        rb.velocity = new Vector2(direction.x, direction.y).normalized * speed;
+
+        float rot = Mathf.Atan2(-direction.y, -direction.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0, 0, rot + 90);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

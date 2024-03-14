5160a525dd6fb57fbd17055e6d354dcb91195221
ivan ramirez <ixr4077@mavs.uta.edu>
2024-03-13 22:17:42 -0500

    First turret working?

--- StealthVania/Assets/Scripts/TurretScript.cs
+++ StealthVania/Assets/Scripts/TurretScript.cs
@@ -6,23 +6,35 @@ public class TurretScript : MonoBehaviour
 {
     public GameObject bullet;
     public Transform bulletPos;
+    // public Transform roomPos;
+    private GameObject player;
+    [SerializeField] private BoxCollider2D room;
+    [SerializeField] private LayerMask playerLayer;
+    
 
     private float timer;
     // Start is called before the first frame update
     void Start()
     {
+        player = GameObject.FindGameObjectWithTag("Player");
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        timer += Time.deltaTime;
-        if (timer > .5f)
+        if (room.IsTouchingLayers(playerLayer))
         {
-            timer = 0;
-            shoot();
+            timer += Time.deltaTime;
+            if (timer > .5f)
+            {
+                timer = 0;
+                shoot();
+            }
+
         }
+
+
     }
     void shoot()
     {
